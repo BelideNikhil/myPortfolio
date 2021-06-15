35b0c512af75ec0142a23a7d7737c99b7cd1f58c
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-06-16 03:07:48 +0530

    add projects main-anime
    added projects page, main page animation, modified main page padding

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-var toggled=document.querySelector("#switcher");
+var buttOn=document.querySelector("#switcher");
 
-toggled.addEventListener("click", function(){
-    console.log("working")
-    document.body.classList.toggle("dark_mode");
-    if(document.body.classList.contains("dark_mode")){
+// defining a key in browser and setting default
+let locale=localStorage.getItem("darkmode")
+console.log(locale)
+
+
+function enableDark(){
+    document.body.classList.add("dark_mode");
     switcher.src="./images/sun.png";
+    localStorage.setItem("darkmode",'enabled-state')
+}
+
+function disableDark(){
+    document.body.classList.remove("dark_mode");
+    switcher.src="./images/moon.png";
+    localStorage.setItem("darkmode",'disabled-state')
+}
+
+// checking on click and changing
+buttOn.addEventListener("click", function(){
+    locale=localStorage.getItem("darkmode");
+    if(locale!='enabled-state'){
+        enableDark()
     }
     else{
-    switcher.src="./images/moon.png";
+        disableDark()
     }
 })
+
+// keeping it enabled on page reload
+if(locale==='enabled-state'){
+    enableDark()
+}
